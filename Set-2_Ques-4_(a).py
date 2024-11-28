12f931c3f51ab5a9a0be5655c536e047bbd18a6f
Jobair Hossain <72722835+jobairh@users.noreply.github.com>
2024-11-28 16:55:57 +0000

    First Commit

---
+++ Set-2_Ques-4_(a).py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+df = pd.read_csv('sales.csv')
+print("Dataset loaded:\n", df.head())
+
+missing_values = df[['Quantity', 'UnitPrice']].isnull().sum()
+print("Checking for missing values:\n", missing_values)
+
+df['Quantity'].fillna(df['Quantity'].median(), inplace=True)
+df['UnitPrice'].fillna(df['UnitPrice'].median(), inplace=True)
+
+missing_values_after = df[['Quantity', 'UnitPrice']].isnull().sum()
+print("Missing values after handling:\n", missing_values_after)
+
+df['OrderDate'] = pd.to_datetime(df['OrderDate'], errors='coerce')
+df['Year'] = df['OrderDate'].dt.year 
+
+print("Data with 'Year' extracted from 'OrderDate'\n:", df.head())
